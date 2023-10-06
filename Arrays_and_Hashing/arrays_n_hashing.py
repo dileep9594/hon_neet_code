2db64b0bf8c7e85651d47624b84cad416a075628
Dileep Pandey <dileep9594@gmail.com>
2023-10-06 12:15:10 +0530

    two pointers completed except hard one

--- Arrays_and_Hashing/arrays_n_hashing.py
+++ Arrays_and_Hashing/arrays_n_hashing.py
@@ -107,4 +107,4 @@ def LongestConsecutiveSequence(nums):
 
 
 if __name__ == "__main__": 
-    print(LongestConsecutiveSequence([100,4,200,1,3,2]))
+    print(LongestConsecutiveSequence([100,4,200,1,3,2])))
