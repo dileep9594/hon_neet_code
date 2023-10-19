18a168b21350e97e25779800bc7356d26e26fa9b
Dileep Pandey <dileep9594@gmail.com>
2023-10-19 11:24:52 +0530

    sliding window all medium level questions solved

--- sliding_window/slidingWindow.py
+++ sliding_window/slidingWindow.py
@@ -1,15 +1,75 @@
+from ast import List
+
+
 class SlidingWindow :
-    def bestTimetoBuyAndSellStock(self):
-        pass
+    def bestTimetoBuyAndSellStock(self,prices:List[int]) -> int:
+        l =0
+        profit =0
+        for r in range(1,len(prices)):
+            if prices[l]<prices[r]:
+                profit = max(profit,prices[r]-prices[l])
+            else :
+                l=r
+        return profit
+    def longestSubstringWithoutRepeatingCharacters(self,s:str) -> int:
+        subString  = set()
+        l =0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in subString :
+                subString.remove(s[l])
+                l += 1
+            subString.add(s[r])
 
-    def longestSubstringWithoutRepeatingCharacters(self):
-        pass
+            res = max(res,r-l+1)
+        
+        return res
 
-    def longestrepeatingCharacterReplacement(self):
-        pass
+        
+        
 
-    def permutationInString(self):
-        pass
+    def longestrepeatingCharacterReplacement(self,  s:str,k:int):
+        count = {}
+        res =0
+        l =0
+        for r in range(len(s)) :
+            count[s[r]] = 1+count.get(s[r],0)
+            while (r-l+1) - max(count.values()) >k :
+                count[s[l]]-=1
+                l+=1
+            res = max(res,r-l+1)
+        return res
+
+
+    def permutationInString(self,s1,s2):
+        if len(s1) > len(s2) : return False
+        s1Count ,s2Count =[0]*26 ,[0]*26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i] - ord('a'))] +=1
+            s2Count[ord(s2[i] - ord('a'))] +=1
+
+        matches =0 
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        l=0 
+        for r in range(len(s1),len(s2)):
+            if matches == 26 : return True
+
+            index = ord(s2[r]-ord('a'))
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index]+1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]-ord('a'))
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index]-1 == s2Count[index]:
+                matches -= 1
+            l+=1
+        return matches == 26
 
     def minimumWindowSubstring(self):
         pass
--- two_pointer/two_pointer.py
+++ two_pointer/two_pointer.py
@@ -65,6 +65,19 @@ def containerWithMostWater(height :List[int]) -> int:
             r-=1
     return mostWater
 
-def trappingRainWater():
-    pass
+def trappingRainWater(height :List[int]) -> int:
+    l,r = 0,len(height)-1
+    leftMax ,rightMax  = height[l] ,height[r]
+    res =0
+    while l<r :
+        if height[l]<height[r] :
+            l+= 1
+            leftMax = max(height[l],leftMax)
+            res+= leftMax-height[l]
+        else :
+            r-=1
+            rightMax = max(rightMax,height[r])       
+            res += rightMax-height[r]
+
+    return res
 
