391eef5cc659aa7aece26ab2f300d66e7f28fff2
Dileep Pandey <dileep9594@gmail.com>
2024-01-13 17:29:16 +0530

    ma area of island

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,5 +1,7 @@
 from collections import deque
 import collections
+class Node(object):
+    pass
 
 class Graph:
     def __init__(self, graph):
@@ -35,16 +37,50 @@ class Graph:
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == "1" and (r,c) not in self.visited :
-                    self.bfs(grid,r,c) 
                     numOfIsland+=1
+                    self.bfs(grid,r,c) 
+                    
         return numOfIsland
+    
+    def noOfConnectedComponents(self,graph):
+        noOfConnectedComponents =0 
+        for node in graph:
+            if node not in self.visited :
+                self.dfs(graph,node)
+                noOfConnectedComponents+=1
+        return noOfConnectedComponents
+
 
 
     def cloneGraph(self, node):
-        pass
+        oldToNew = {}
+        def dfs_helper(node):
+            if node in oldToNew :
+                return oldToNew[node] 
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for node in node.neighbors :
+                copy.neighbors.append(dfs_helper(node))
+            return copy
+        return dfs_helper(node) if node else None
+
 
     def maxAreaOfIsland(self, grid):
-        pass
+        row,col = len(grid) , len(grid[0])
+        answer = 0
+        visited = set()
+
+        def dfs(r,c):
+            if r<0 or r>= row or c< 0 or c>=col or grid[r][c] ==0 or (r,c) in visited:
+                return 0
+            visited.add((r,c))
+            return (1+dfs(r+1,c)+dfs(r-1,c)+dfs(r,c+1)+dfs(r,c-1))      
+
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c]==1 :
+                    answer = max(answer,dfs(r,c))
+        return answer
 
     def pacificAtlantic(self, matrix):
         pass
@@ -85,13 +121,16 @@ if __name__ == '__main__':
     # for (u,v) in edges :
     #     gr[u].append(v)
     #     gr[v].append(u)
-    gr = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
-]
-    graph = Graph(gr)
+#     gr = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+    grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+    graph = Graph(grid)
     # graph.printGraph(gr)  # print the graph
     # print(graph.dfs(gr,1)) # Depth frist search of the graph
-    print(graph.numIslands(gr))
+    # print(graph.numIslands(gr))
+    print(graph.maxAreaOfIsland(grid))
+
